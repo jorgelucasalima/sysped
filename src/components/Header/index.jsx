9e0abf66d0044f9d23a3c6e6d6cb87f1680e4685
jorgelucasalima <jorgelucasalima@gmail.com>
2022-03-18 15:50:39 -0300

    inclusão do menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,18 @@
 import {FiPackage} from 'react-icons/fi';
-import {Container} from './styles'
+import {Container, Content, ContentMenu} from './styles'
 
 export function Header() {
   return (
     <Container>
-      <FiPackage/>
-      <button type='button'>Cadastrar Pedido</button>
+      <ContentMenu>
+        <a>Empresa</a>
+        <a>Produtos</a>
+        <a>Funcionários</a>
+      </ContentMenu>
+      <Content>
+        <FiPackage size={35}/>
+        <button type='button'>Cadastrar Pedido</button>
+      </Content>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,3 +3,56 @@ import styled from 'styled-components'
 export const Container = styled.header`
   background: var(--azul);
 `
+
+export const ContentMenu = styled.div`
+
+  max-width: 1120px;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+
+  a {
+    color: #FFFFFF;
+    font-size: 1.15rem;
+    padding: 1rem;
+    margin-left: 1rem;
+
+    cursor: pointer;
+  }
+`
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 1rem 1rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  svg {
+    color: white;
+  }
+
+  button {
+    font-size: 1rem;
+    color: #FFFFFF;
+    background: var(--azul-claro);
+    border: 0;
+    padding: 0 2rem;
+    height: 3rem;
+    border-radius: 0.25rem;
+   
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+
+  }
+
+
+
+
+`
