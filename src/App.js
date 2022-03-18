e20e9dd97164b48083e14c54047df2849fed24a3
jorgelucasalima <jorgelucasalima@gmail.com>
2022-03-17 21:19:33 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import EstiloGlobal from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <p>pagina app</p>
+      <EstiloGlobal/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  
+  :root{
+    --cordefundo: #F0F2F5;
+    --vermelho: #e52e4d;
+    --azul: #5429cc;
+    --azul-claro: #6933ff;
+    
+    --verde: #33CC95;
+    --texto-title: #363f5f;
+    --texto-body: #969cb3;
+    --fundo: #F0F2F5;
+    --shape: #FFFFFF;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; 
+  }
+
+  html {
+    @media (max-width: 1088px) {
+      font-size: 93.75%; //15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+  }
+
+  body {
+    background: var(--backgroud);
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+  button {
+    cursor: pointer;
+  }
+  [disabled] {
+    opacity: 8.6;
+    cursor: not-allowed;
+  }
+
+`
+
+export default EstiloGlobal
