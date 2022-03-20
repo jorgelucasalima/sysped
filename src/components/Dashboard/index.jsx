26233e4baa1069715e997b864884f8db303dc5d3
jorgelucasalima <jorgelucasalima@gmail.com>
2022-03-20 09:40:48 -0300

    criado dash e resumo do dash

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+
+export default function Dashboard() {
+  return (
+    <Container>
+      Dashboard
+    </Container>
+  )
+}
---
+++ src/components/Resumo/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export default function Resumo() {
+  return (
+    <Container>
+      Resumo
+    </Container>
+  );
+}
+
