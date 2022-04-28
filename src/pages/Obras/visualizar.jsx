a53bbc19b091dd030dd57d3901afab532e314610
jorgelucasalima <jorgelucasalima@gmail.com>
2022-04-28 09:41:16 -0300

    correção de bug ao visualizar obra

--- src/pages/Obras/visualizar.jsx
+++ src/pages/Obras/visualizar.jsx
@@ -2,6 +2,7 @@ import { HeaderMenu } from "../../components/HeaderMenu";
 import {useParams} from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { Container } from "./styles";
+import firebase from '../../services/firebaseConnection'
 
 
 
@@ -9,22 +10,37 @@ export function VisualizarObra() {
 
   const { id } = useParams();
 
-  const [obra, setObra] = useState();
-  const [responsavelObras, setResponsavelObras] = useState('')
+  const [obra, setObra] = useState({});
   console.log(obra)
-  console.log(id)
-  
+
+
+  useEffect(() => {
+    async function loadObra() {
+      await firebase.firestore().collection(`obras`)
+      .doc(id)
+      .get({
+        obras: obra.obras,
+        responsavelObras: obra.responsavelObras
+      })
+      .then(response => {
+        setObra(response.data())
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+    loadObra();
+  }, [id]);
+
 
   return (
     <>
       <HeaderMenu />
       <Container>
-
-    
         <h1>Obra:</h1>
-        <h2></h2>
+        <h2>{obra.obras}</h2>
         <h1>Responsável:</h1>
-        <h2>Mano</h2>
+        <h2>{obra.responsavelObras}</h2>
       </Container>
       
     </>
